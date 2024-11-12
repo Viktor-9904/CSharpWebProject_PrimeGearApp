b0795d16d1d359bd7a08054932b524f7f8d19da4
Viktor Stanev <viktorstanev192@gmail.com>
2024-11-12 19:43:16 +0200

    Added ProductDetails Model.

---
+++ PrimeGearApp.Data.Models/ProductDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace PrimeGearApp.Data.Models
+{
+    public class ProductDetails
+    {
+        public Guid Id { get; set; }
+           = Guid.NewGuid();
+        public Guid ProductId { get; set; }
+        public Guid ProductTypeProperityId { get; set; }
+        public string ProductTypePropertyValue { get; set; } = null!;   
+    }
+}
