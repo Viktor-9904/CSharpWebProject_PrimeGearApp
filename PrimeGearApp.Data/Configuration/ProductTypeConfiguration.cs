6c33ee522199a68e4f175e2fa7981b9de3f095e7
Viktor Stanev <viktorstanev192@gmail.com>
2024-11-15 15:27:34 +0200

    Added ProductType Seeder.

--- PrimeGearApp.Data/Configuration/ProductTypeConfiguration.cs
+++ PrimeGearApp.Data/Configuration/ProductTypeConfiguration.cs
@@ -18,6 +18,88 @@ namespace PrimeGearApp.Data.Configuration
                 .IsRequired()
                 .HasComment("ProductType Name")
                 .HasMaxLength(ProductTypeNameMaxLength);
+
+            builder
+                .HasData(this.SeedProductTypes());
+        }
+
+        private IEnumerable<ProductType> SeedProductTypes()
+        {
+
+            List<ProductType> productTypes = new List<ProductType>()
+            {
+                new ()
+                {
+                    Name = "CPU"
+                },
+                new ()
+                {
+                    Name = "GPU"
+                },
+                new ()
+                {
+                    Name = "Motherboard"
+                },
+                new ()
+                {
+                    Name = "RAM"
+                },
+                new ()
+                {
+                    Name = "Power Supply"
+                },
+                new ()
+                {
+                    Name = "HDD"
+                },
+                new ()
+                {
+                    Name = "SSD"
+                },
+                new ()
+                {
+                    Name = "Cooling Fan"
+                },
+                new ()
+                {
+                    Name = "CPU Fan Cooler"
+                },
+                new ()
+                {
+                    Name = "CPU AIO Cooler"
+                },
+                new ()
+                {
+                    Name = "PC Case"
+                },
+                new ()
+                {
+                    Name = "Monitor"
+                },
+                new ()
+                {
+                    Name = "Monitor Stand"
+                },
+                new ()
+                {
+                    Name = "Keyboard"
+                },
+                new ()
+                {
+                    Name = "Mouse"
+                },
+                new ()
+                {
+                    Name = "Headset"
+                },
+                new ()
+                {
+                    Name = "Mouse Pad"
+                }
+
+            };
+
+            return productTypes;
         }
     }
 }
