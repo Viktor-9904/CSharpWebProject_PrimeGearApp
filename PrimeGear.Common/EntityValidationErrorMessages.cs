24dafbd63b446523c630c834bf0fc5a52a734bc7
Viktor Stanev <viktorstanev192@gmail.com>
2024-11-26 13:30:46 +0200

    Added custom error messeages in create view.

--- PrimeGear.Common/EntityValidationErrorMessages.cs
+++ PrimeGear.Common/EntityValidationErrorMessages.cs
@@ -4,24 +4,32 @@
     {
         public static class ProductErrorMessages
         {
+            public const string NameIsRequired = "Name is required!";
             public const string NameIsTooShort = "Name is too short!";
             public const string NameIsTooLong = "Name is too long!";
 
+            public const string BrandIsRequired = "Brand is required!";
             public const string BrandIsTooShort = "Brand is too short!";
             public const string BrandIsTooLong = "Brand is too long!";
 
+            public const string ReleaseDateIsRequired = "Release date is required!";
+
+            public const string PriceIsRequired = "Price is required!";
             public const string PriceIsTooLow = "Price is too low!";
             public const string PriceIsTooHigh = "Brand is too high!";
             public const string PriceNotInRange = "Price not in range!";
 
+            public const string WarrantyIsRequired = "Warranty is required!";
             public const string WarrantyMin = "Warranty can't be below 0!";
             public const string WarrantyMax = "Warranty is too high!";
             public const string WarrantyNotInRange = "Warranty not in range!";
 
+            public const string QuantityIsRequired = "Available quantity is required!";
             public const string QuantityMin = "Avaible quantity can't be below 0!";
             public const string QuantityMax = "Avaible quantity is too high!";
             public const string QuantityNotInRange = "Quantity not in range!";
 
+            public const string ProductTypeSelectionRequired = "Product Type is required!";
         }
     }
 }
--- PrimeGear.Web.ViewModels/ProductViewModels/CreateProductViewModel.cs
+++ PrimeGear.Web.ViewModels/ProductViewModels/CreateProductViewModel.cs
@@ -10,39 +10,44 @@ namespace PrimeGearApp.Web.ViewModels.ProductViewModels
 {
     public class CreateProductViewModel
     {
-        [Required]
+        [Required(ErrorMessage = NameIsRequired)]
         [MinLength(ProductNameMinLength, ErrorMessage = NameIsTooShort)]
         [MaxLength(ProductNameMaxLength, ErrorMessage = NameIsTooLong)]
         public string Name { get; set; } = null!;
 
-        [Required]
+        [Required(ErrorMessage = BrandIsRequired)]
         [MinLength(ProductBrandMinLength, ErrorMessage = BrandIsTooShort)]
         [MaxLength(ProductBrandMaxLength, ErrorMessage = BrandIsTooLong)]
         public string Brand { get; set; } = null!;
 
+        [Required(ErrorMessage = ReleaseDateIsRequired)]
         public DateTime ReleaseDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = PriceIsRequired)]
         [Range((double)ProductMinPrice, (double)ProductMaxPrice, ErrorMessage = PriceNotInRange)] // TODO: Implement custom attribute for too low or too high
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage = PriceIsRequired)]
         [Range(ProductMinWarrantyDurationInMonths, ProductMaxWarrantyDurationInMonths, ErrorMessage = WarrantyNotInRange)]
         public int WarrantyDurationInMonths { get; set; }
 
+        [Required(ErrorMessage = QuantityIsRequired)]
         [Range(ProductMinAvaibleQuantity, ProductMaxAvaibleQuantity, ErrorMessage = QuantityNotInRange)]
         public int AvaibleQuantity { get; set; }
 
         public IFormFile? ProductImagePath { get; set; }
+
+        [Required(ErrorMessage = ProductTypeSelectionRequired)]
         public int SelectedProductTypeId { get; set; } // For dropdown selection
+
         public IEnumerable<ProductTypeViewModel> ProductTypes { get; set; } 
             = new List<ProductTypeViewModel>();
+
         public IEnumerable<ProductTypePropertyViewModel> ProductTypeProperties { get; set; } 
             = new List<ProductTypePropertyViewModel>();
 
         [Required]
-        public Dictionary<int, string> ProductProperties { get; set; } = new Dictionary<int, string>();
-
-         
-
+        public Dictionary<int, string> ProductProperties { get; set; }
+            = new Dictionary<int, string>();      
     }
 }
