4bb4360e21bc8839106d0386d90f6f3856bdc0fd
Viktor Stanev <viktorstanev192@gmail.com>
2024-12-04 16:30:59 +0200

    Added Manager entity.

---
+++ PrimeGearApp.Data.Models/Manager.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimeGearApp.Data.Models
+{
+    public class Manager
+    {
+        public Guid Id { get; set; }
+        public string WorkPhoneNumber { get; set; } = null!;
+        public Guid UserId { get; set; }
+        public virtual ApplicationUser User { get; set; } = null!;
+    }
+}
