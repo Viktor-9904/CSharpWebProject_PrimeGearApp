099d97109443c354ecda7062b653aa43b25b44cd
Viktor Stanev <viktorstanev192@gmail.com>
2024-12-05 21:34:29 +0200

    Added Cart and Order controllers.

---
+++ PrimeGearApp.Web/Controllers/CartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeGearApp.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PrimeGearApp.Web/Controllers/OrdersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeGearApp.Web.Controllers
+{
+    public class OrdersController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
